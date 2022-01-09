93e5d9328f4671b900571e67ae11808b08b1d2c6
leelk <karunarathne1995@gmail.com>
2022-01-10 00:28:46 +0530

    add for App.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(3000, function () {
+    console.log("Express server listening on port 3000");
+    });
+
+
+app.get('/', (req, res)=>{
+    res.send('<h1> Express Demo <h1> <h4> Message: Success</h4><p> version 0.0.1');
+})
